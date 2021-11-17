3dc05076daabed38476a647294f871588a76ff07
Juan Felipe Varner <Juan_Varner@ryder.com>
2021-11-17 16:21:13 -0500

    Add project files.

---
+++ PWAPhotos/Controllers/ImagesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PWAPhotos.Data;
+using PWAPhotos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWAPhotos.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImagesController : ControllerBase
+    {
+        
+        private readonly ILogger<ImagesController> _logger;
+        private readonly DBContext  _cntx;
+
+        public ImagesController(ILogger<ImagesController> logger, DBContext cntx)
+        {
+            _cntx = cntx;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<List<ImageDTO>> Get()
+        {
+            var images = await _cntx.Images.ToListAsync();
+            return images;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ImageDTO image)
+        {
+            try
+            {
+                await _cntx.Images.AddAsync(image);
+                await _cntx.SaveChangesAsync();
+            }
+            catch (Exception e )
+            {
+                throw;
+            }
+           
+            return Ok(image);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var image = await _cntx.Images.FindAsync(Id);
+            _cntx.Images.Remove(image);
+            await _cntx.SaveChangesAsync();
+            return Ok("Id");
+        }
+    }
+}
---
+++ PWAPhotos/Data/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PWAPhotos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWAPhotos.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ImageDTO> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ImageDTO>().ToTable("Images");
+        }
+    }
+}
